d6e8f217265aeb20f1d78e31809e8fd97af2f4dd
ramintheredmn <redcrossprog@gmail.com>
2025-02-25 02:14:08 +0330

    sneklang object 2

--- obj/main.c
+++ obj/main.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "simpleobj.h"
 
-langObj_t* new_ptr_obj(int value) {
+langObj_t* new_ptr_obj(char* value) {
     langObj_t* new_ptr_obj = malloc(sizeof(langObj_t));
     if (new_ptr_obj == NULL) {return NULL;};
 
-    new_ptr_obj->value.v_int = value;
-    new_ptr_obj->kind = INTEGER;
+    new_ptr_obj->value.v_string = malloc(strlen(value) + 1);
+    strcpy(new_ptr_obj->value.v_string, value);
+    new_ptr_obj->kind = STRING;
+
+
 
     return new_ptr_obj;
 }
 
 int main(void) {
-    langObj_t* lang_obj = new_ptr_obj(69);
+    langObj_t* lang_obj = new_ptr_obj("Ramin was here!!!!!");
 
-    printf("Hello with new object!, %i", lang_obj->value);
+    printf("Hello with new object!, %s", lang_obj->value);
 
 }
--- obj/simpleobj.h
+++ obj/simpleobj.h
@@ -1,12 +1,35 @@
+#include <stdlib.h>
+
+typedef struct langObj langObj_t;
+
 typedef enum lang_kind {
-	INTEGER
+	INTEGER,
+	STRING,
+	VECTOR,
+	LIST
+
 } lang_obj_kind_t;
+
+typedef struct list {
+	size_t size;
+	langObj_t** data;
+} list_t;
+
+typedef struct vector {
+	langObj_t* x;
+	langObj_t* y;
+	langObj_t* z;
+} vector_t;
 
 typedef union lang_value {
 	int v_int;
+	char* v_string;
+	vector_t v_vector;
+	list_t v_list;
 } lang_obj_value_t;
 
 typedef struct langObj {
 	lang_obj_kind_t kind;
 	lang_obj_value_t value;
 } langObj_t;
+
